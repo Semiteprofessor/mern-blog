0965b71ad80adb124f50bd9cb8d7b0f29bd59648
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-09 07:44:51 +0100

    Commit

--- api/migrations/20240915063712_create_users_table.js
+++ api/migrations/20240915063712_create_users_table.js
@@ -1,22 +1,22 @@
-// /**
-//  * @param { import("knex").Knex } knex
-//  * @returns { Promise<void> }
-//  */
-// exports.up = function (knex) {
-//   return knex.schema.createTable("users", (table) => {
-//     table.increments("id").primary();
-//     table.string("name").notNullable();
-//     table.string("email").notNullable().unique();
-//     table.string("password").notNullable();
-//     table.timestamp("createdAt").defaultTo(knex.fn.now());
-//     table.timestamp("updatedAt").defaultTo(knex.fn.now());
-//   });
-// };
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("email").notNullable().unique();
+    table.string("password").notNullable();
+    table.timestamp("createdAt").defaultTo(knex.fn.now());
+    table.timestamp("updatedAt").defaultTo(knex.fn.now());
+  });
+};
 
-// /**
-//  * @param { import("knex").Knex } knex
-//  * @returns { Promise<void> }
-//  */
-// exports.down = function (knex) {
-//   return knex.schema.dropTableIfExists("users");
-// };
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
+};
