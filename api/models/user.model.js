92ba24aa4dd7dc43c9e2dd94a2c1916b8db9c5f4
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-10 07:44:00 +0100

    Commit

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,37 +1,37 @@
-// const { Model } = require("objection");
+const { Model } = require("objection");
 
-// class User extends Model {
-//   static get tableName() {
-//     return "users";
-//   }
+class User extends Model {
+  static get tableName() {
+    return "users";
+  }
 
-//   static get idColumn() {
-//     return "id";
-//   }
+  static get idColumn() {
+    return "id";
+  }
 
-//   static get jsonSchema() {
-//     return {
-//       type: "object",
-//       required: ["name", "email", "password"],
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: ["name", "email", "password"],
 
-//       properties: {
-//         id: { type: "integer" },
-//         name: { type: "string", minLength: 3, maxLength: 20 },
-//         email: { type: "string", format: "email" },
-//         password: { type: "string", minLength: 8, maxLength: 100 },
-//         createdAt: {
-//           type: "string",
-//           format: "date-time",
-//           default: () => new Date().toISOString(),
-//         },
-//         updatedAt: {
-//           type: "string",
-//           format: "date-time",
-//           default: () => new Date().toISOString(),
-//         },
-//       },
-//     };
-//   }
-// }
+      properties: {
+        id: { type: "integer" },
+        name: { type: "string", minLength: 3, maxLength: 20 },
+        email: { type: "string", format: "email" },
+        password: { type: "string", minLength: 8, maxLength: 100 },
+        createdAt: {
+          type: "string",
+          format: "date-time",
+          default: () => new Date().toISOString(),
+        },
+        updatedAt: {
+          type: "string",
+          format: "date-time",
+          default: () => new Date().toISOString(),
+        },
+      },
+    };
+  }
+}
 
-// module.exports = User;
+module.exports = User;
