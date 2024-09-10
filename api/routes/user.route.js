d1867f3a7474a934b08a4063e085dc0142c802d5
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-10 07:43:28 +0100

    Commit

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,14 +1,14 @@
-// const router = require("express").Router();
-// const {
-//   getAllUsers,
-//   editUser,
-//   deleteUser,
-//   getUserById,
-// } = require("../controllers/user.ctrl");
+const router = require("express").Router();
+const {
+  getAllUsers,
+  editUser,
+  deleteUser,
+  getUserById,
+} = require("../controllers/user.ctrl");
 
-// router.get("/", getAllUsers);
-// router.get("/:id", getUserById);
-// router.patch("/:edit-user", editUser);
-// router.delete("/delete-user", deleteUser);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.patch("/:edit-user", editUser);
+router.delete("/delete-user", deleteUser);
 
-// module.exports = router;
+module.exports = router;
