d8bcae24be7c82e57bb6c4533808e2cea493f52d
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-11 08:00:45 +0100

    Commit

---
+++ api/models/blog.model.js
@@ -0,0 +1,50 @@
+const { Model } = require("objection");
+
+class Post extends Model {
+  static get tableName() {
+    return "posts";
+  }
+
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: ["title", "desc", "username"],
+
+      properties: {
+        id: { type: "integer" },
+        title: { type: "string", minLength: 1, maxLength: 255 },
+        desc: { type: "string", minLength: 1 },
+        photo: { type: "string", maxLength: 255 },
+        username: { type: "string", minLength: 1, maxLength: 255 },
+        categories: { type: "array", items: { type: "string" } },
+        createdAt: {
+          type: "string",
+          format: "date-time",
+          default: () => new Date().toISOString(),
+        },
+        updatedAt: {
+          type: "string",
+          format: "date-time",
+          default: () => new Date().toISOString(),
+        },
+      },
+    };
+  }
+
+  static get relationMappings() {
+    const User = require("./User");
+    return {
+      author: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: "posts.username",
+          to: "users.username",
+        },
+      },
+    };
+    return {};
+  }
+}
+
+module.exports = Post;
