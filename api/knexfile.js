6c23fc93a2d036622102869a34b1fd9e9312c3ce
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-11 07:54:30 +0100

    Commit

--- api/knexfile.js
+++ api/knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      password: process.env.DB_PASS,
       database: process.env.DB_NAME,
     },
     migrations: {
